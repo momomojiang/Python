cf859aac9af4303671093ff3481ed27cc1adf74b
momomojiang <41605167+momomojiang@users.noreply.github.com>
2018-12-26 15:48:08 -0800

    Add files via upload

---
+++ print_String.py
@@ -0,0 +1,20 @@
+print( "Hello World" )
+
+name = "Steve"
+print("Hello", name ,"!")
+
+name = "Eddie"
+print("Hello "+ name +" !")
+
+x="5"
+print("Age:",x)
+
+x="6"
+print("Age: "+ x)
+
+
+
+y="pizza"
+z="burger"
+print("I love to eat {} and {}.".format(z, y))
+print(f"I love to eat {y} and {z}.")
